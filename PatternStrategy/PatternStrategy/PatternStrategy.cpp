c477ca288d69d3cf366773556e9c605a3c9511da
ASPER <144143767+ASPERh@users.noreply.github.com>
2024-06-21 20:12:26 +0100

    Add files via upload

---
+++ PatternStrategy/PatternStrategy/PatternStrategy.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ShippingStrategy {
+public:
+    virtual double calculate(double orderAmount) const = 0;
+    virtual ~ShippingStrategy() {}
+};
+
+class RegularMailStrategy : public ShippingStrategy {
+public:
+    double calculate(double orderAmount) const override {
+        return 5.0;
+    }
+};
+
+class ExpressDeliveryStrategy : public ShippingStrategy {
+public:
+    double calculate(double orderAmount) const override {
+        return 10.0 + (0.1 * orderAmount);
+    }
+};
+
+class ShippingContext {
+private:
+    ShippingStrategy* strategy;
+
+public:
+    ShippingContext(ShippingStrategy* strategy) : strategy(strategy) {}
+
+    void setStrategy(ShippingStrategy* newStrategy) {
+        strategy = newStrategy;
+    }
+
+    double calculateShippingCost(double orderAmount) const {
+        if (strategy) {
+            return strategy->calculate(orderAmount);
+        }
+        return 0.0;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    RegularMailStrategy regularMailStrategy;
+    ShippingContext shippingContext(&regularMailStrategy);
+
+    double shippingCost = shippingContext.calculateShippingCost(50.0);
+    cout << "Стоимость доставки (обычная почта): $" << shippingCost << "\n";
+
+    ExpressDeliveryStrategy expressDeliveryStrategy;
+    shippingContext.setStrategy(&expressDeliveryStrategy);
+
+    shippingCost = shippingContext.calculateShippingCost(100.0);
+    cout << "Стоимость доставки (экспресс): $" << shippingCost << "\n";
+
+    return 0;
+}
